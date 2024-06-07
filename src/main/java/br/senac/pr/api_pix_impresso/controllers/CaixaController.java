87a7164fdef2dda51c34b3af306469f92489b6ad
Guilherme-Zatta <guilhermezatta@hotmail.com>
2024-06-06 21:25:33 -0300

    Aula 06.06

---
+++ src/main/java/br/senac/pr/api_pix_impresso/controllers/CaixaController.java
@@ -0,0 +1,40 @@
+package br.senac.pr.api_pix_impresso.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.senac.pr.api_pix_impresso.dtos.CreateCaixaDto;
+import br.senac.pr.api_pix_impresso.models.Caixa;
+import br.senac.pr.api_pix_impresso.services.CaixaService;
+
+@RestController
+@RequestMapping("/caixas")
+public class CaixaController {
+    private CaixaService CaixaService;
+
+    public CaixaController(CaixaService caixaService) {
+        CaixaService = caixaService;
+    }
+
+    @GetMapping("")
+    public List<Caixa> gatCaixas() {
+        return CaixaService.findAll();
+    }
+
+    @PostMapping("")
+    public Caixa createCaixa(@RequestBody CreateCaixaDto dto) {
+        Caixa caixa = new Caixa(null,
+            dto.getLocalizacao(),
+            dto.getSaldo());
+        // Salvar no banco
+        int id = CaixaService.save(caixa);
+        caixa.setId(Long.valueOf(id));
+        // retornar o objeto caixa o id
+        return caixa;
+    }
+}
--- src/main/java/br/senac/pr/api_pix_impresso/repositories/JdbcCaixaRepository.java
+++ src/main/java/br/senac/pr/api_pix_impresso/repositories/JdbcCaixaRepository.java
@@ -1,17 +1,20 @@
-package br.senac.pr.api_pix_impresso.repository;
+package br.senac.pr.api_pix_impresso.repositories;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import br.senac.pr.api_pix_impresso.models.Caixa;
+
 @Repository
 public class JdbcCaixaRepository implements CaixaRepository{
 
-  @Autowired
   private JdbcTemplate jdbcTemplate;
+
+    public JdbcCaixaRepository(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+}
 
     @Override
     public int save(Caixa caixa) {
@@ -40,8 +43,13 @@ public class JdbcCaixaRepository implements CaixaRepository{
 
     @Override
     public List<Caixa> findAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+        return jdbcTemplate
+            .query("SELECT ID, LOCALIZACAO, SALDO FROM CAIXAS", 
+                (rs, rowNum) -> {
+                    return new Caixa(rs.getLong("ID"),
+                        rs.getString("LOCALIZACAO"),
+                        rs.getDouble("SALDO"));
+        });
     }
 
     @Override

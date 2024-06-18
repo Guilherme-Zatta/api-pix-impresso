8fc3c5029081cb2c3f86d3a8eb0c0c972c5f88f1
Guilherme-Zatta <guilhermezatta@hotmail.com>
2024-06-18 19:21:01 -0300

    18.06.2024 - 19:20

--- src/main/java/br/senac/pr/api_pix_impresso/dtos/UpdateCaixaDto.java
+++ src/main/java/br/senac/pr/api_pix_impresso/dtos/UpdateCaixaDto.java
@@ -1,29 +1,32 @@
 package br.senac.pr.api_pix_impresso.dtos;
 
+
+// DTO - Data Transfer Object
+// Objeto que serve para transferir dados entre
+// classes ou etapas de execução
 public class UpdateCaixaDto {
-    private String localizacao;
-    private Double saldo;
-   
-    public UpdateCaixaDto(String localizacao, Double saldo) {
-        this.localizacao = localizacao;
-        this.saldo = saldo;
-    }
+  private String localizacao;
+  private Double saldo;
 
-    public String getLocalizacao() {
-        return localizacao;
-    }
+  public UpdateCaixaDto(String localizacao, Double saldo) {
+    this.localizacao = localizacao;
+    this.saldo = saldo;
+  }
 
-    public void setLocalizacao(String localizacao) {
-        this.localizacao = localizacao;
-    }
+  public String getLocalizacao() {
+    return localizacao;
+  }
 
-    public Double getSaldo() {
-        return saldo;
-    }
+  public void setLocalizacao(String localizacao) {
+    this.localizacao = localizacao;
+  }
 
-    public void setSaldo(Double saldo) {
-        this.saldo = saldo;
-    }
+  public Double getSaldo() {
+    return saldo;
+  }
 
+  public void setSaldo(Double saldo) {
+    this.saldo = saldo;
+  }
 
 }
